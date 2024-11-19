e76318b2d895d8d564db145a6a4716fcfb3c090f
Mykola Makhin <mykola_makhin@epam.com>
2024-11-20 00:04:20 +0200

    Implemented calls to EPAM DIAL via SemanticKernel API.

---
+++ src/main/java/com/epam/training/gen/ai/controller/ChatController.java
@@ -0,0 +1,34 @@
+package com.epam.training.gen.ai.controller;
+
+import com.epam.training.gen.ai.dto.ChatInputDto;
+import com.epam.training.gen.ai.dto.ChatOutputDto;
+import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.orchestration.InvocationContext;
+import com.microsoft.semantickernel.services.chatcompletion.ChatCompletionService;
+import com.microsoft.semantickernel.services.chatcompletion.ChatMessageContent;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("chat")
+@RequiredArgsConstructor
+@Slf4j
+public class ChatController {
+
+  private final ChatCompletionService chatCompletionService;
+  private final Kernel kernel;
+  private final InvocationContext invocationContext;
+
+  @PostMapping
+  public ChatOutputDto chat(@RequestBody ChatInputDto input) {
+    var prompt = input.getInput();
+    log.info("Querying with input {}", prompt);
+    var response =
+      chatCompletionService.getChatMessageContentsAsync(prompt, kernel, invocationContext).block().stream()
+        .map(ChatMessageContent::getContent).collect(Collectors.joining("\n\n"));
+    return ChatOutputDto.builder().output(response).build();
+  }
+}
---
+++ src/main/java/com/epam/training/gen/ai/dto/ChatInputDto.java
@@ -0,0 +1,14 @@
+package com.epam.training.gen.ai.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatInputDto {
+  private String input;
+}
